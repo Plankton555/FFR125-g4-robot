44c60aaf0fcd7e88f99d15a54480dbfe0455b731
Paul Lange <palango@gmx.de>
2015-04-01 11:38:15 +0200

    Latest version - Averaging working

--- Exercise1.ino
+++ Exercise1.ino
@@ -1,31 +1,38 @@
 #include <Servo.h>
 
-int forwardSpeed = 40; // interval: [-100, 100]
-int backwardSpeed = -20; // interval: [-100, 100]
-int turningSpeed = 10; // interval: [-100, 100]
-int backingTimephotoDetThreshold = 1000; // milliseconds? 1 second?
-int turningTimephotoDetThreshold = 1000; // milliseconds? 1 second?
+const int forwardSpeed = 40; // interval: [-100, 100]
+const int backwardSpeed = -20; // interval: [-100, 100]
+const int turningSpeed = 10; // interval: [-100, 100]
+const unsigned long backingTimeThreshold = 1000; // milliseconds?
+const unsigned long turningTimeThreshold = 2000; // milliseconds?
 const int speakerPin = 4;
 const int redLEDPin = 13;
 const int leftEyePin = A3;
 const int rightEyePin = A5;
 const int leftWhiskerPin = 8;
 const int rightWhiskerPin = 9;
-int motorLPin = 11;
-int motorRPin = 12;
+const int motorLPin = 11;
+const int motorRPin = 12;
 
 // FSM: 0 = search for mines, 1 = avoid obstacle, 2 = mine detected???
 int currentState = 0;
-int backingStartTime = 0;
+unsigned long backingStartTime = 0;
 bool isCurrentlyBacking = false;
-int turningStartTime = 0;
+unsigned long turningStartTime = 0;
 bool isCurrentlyTurning = false;
 
 Servo ServoL;
 Servo ServoR;
+int turningDirection = 1;
 
 // photodetector-related variables
-double photoDetThreshold;
+float photoDetThreshold;
+float leftEyeAvg;
+float rightEyeAvg;
+float decayRate = 1 - 1 / 10.0;
+float detectThreshold = 0.5;
+bool leftDetect;
+bool rightDetect;
 
 // IR-related variables
 int distance;
@@ -46,8 +53,11 @@ void setup() // Built-in initialization block
 
 void loop() // Main loop auto-repeats
 {
+  Serial.print("current state: ");
+  Serial.println(currentState);
   // get sensor readings
   bool obstacleDetected = detectObstacle();
+  updateEyes();
   bool mineDetected = detectMine();
 
 
@@ -56,48 +66,59 @@ void loop() // Main loop auto-repeats
 
     if (obstacleDetected) {
       currentState = 1; // avoid the obstacle
-    } else if (mineDetected) {
+      
+      turningDirection = digitalRead(rightWhiskerPin) - digitalRead(leftWhiskerPin);
+      if (turningDirection == 0){
+        turningDirection = -1;
+      }
+      
+    } 
+    else if (mineDetected) {
       currentState = 2; // do something with mine
-    } else { // if nothing detected, move forward
+    } 
+    else { // if nothing detected, move forward
       ServoL.writeMicroseconds(convertSpeedL(forwardSpeed));
       ServoR.writeMicroseconds(convertSpeedR(forwardSpeed));
     }
-  } else if (currentState == 1) { // avoid the obstacle
+  } 
+  else if (currentState == 1) { // avoid the obstacle
     stopRobot();
     currentState = 3;
-  } else if (currentState == 2) { // do something with mine
+    backingStartTime = millis();
+  } 
+  else if (currentState == 2) { // do something with mine
     stopRobot();
     currentState = 3;
-  } else if (currentState == 3) { // back the robot up
-    if (!isCurrentlyBacking) {
-      backingStartTime = millis();
-      isCurrentlyBacking = true;
-    } else {
-      if (millis() - backingStartTime < backingTimephotoDetThreshold) {
-        ServoL.writeMicroseconds(convertSpeedL(backwardSpeed));
-        ServoR.writeMicroseconds(convertSpeedR(backwardSpeed));
-      } else {
-        isCurrentlyBacking = false;
-        currentState = 4; // turn the robot
-      }
-    }
-  } else if (currentState == 4) { // turn the robot
-    if (!isCurrentlyTurning) {
+    tone(speakerPin, 4000, 100); // Play tone for 1 second
+    delay(100); // Delay to finish tone
+    backingStartTime = millis();
+  } 
+  else if (currentState == 3) { // back the robot up
+    if (millis() - backingStartTime < backingTimeThreshold) {
+      ServoL.writeMicroseconds(convertSpeedL(backwardSpeed));
+      ServoR.writeMicroseconds(convertSpeedR(backwardSpeed));
+    } 
+    else {
+      isCurrentlyBacking = false;
+      currentState = 4; // turn the robot
       turningStartTime = millis();
-      isCurrentlyTurning = true;
-    } else {
-      if (millis() - turningStartTime < turningTimephotoDetThreshold) {
-        ServoL.writeMicroseconds(convertSpeedL(turningSpeed));
-        ServoR.writeMicroseconds(convertSpeedR(-turningSpeed));
-      } else {
-        isCurrentlyTurning = false;
-        currentState = 0; // move forward agaidn
-      }
+    }
+
+  } 
+  else if (currentState == 4) { // turn the robot
+    if (millis() - turningStartTime < turningTimeThreshold) {
+      ServoL.writeMicroseconds(convertSpeedL(turningSpeed*turningDirection));
+      ServoR.writeMicroseconds(convertSpeedR(-turningSpeed*turningDirection));
+    } 
+    else {
+      isCurrentlyTurning = false;
+      currentState = 0; // move forward agaidn
+
     }
   }
 
 
-  delay(100);
+  delay(50);
 }
 
 void stopRobot() {
@@ -108,10 +129,45 @@ void stopRobot() {
 
 bool detectObstacle() {
   return !digitalRead(leftWhiskerPin) || !digitalRead(rightWhiskerPin);
+
+}
+
+void updateEyes() {
+  float leftEye = volts(leftEyePin);
+  float rightEye = volts(rightEyePin);
+  leftEyeAvg = leftEyeAvg * decayRate + leftEye * (1 - decayRate);
+  Serial.println(leftEye);
+  Serial.println(leftEyeAvg);
+  Serial.println(' ');
+  rightEyeAvg = rightEyeAvg * decayRate + rightEye * (1 - decayRate);
+  leftDetect = leftEyeAvg * detectThreshold > leftEye;
+  rightDetect = rightEyeAvg * detectThreshold > rightEye;
   
 }
 
+bool detectMine() {
+  return (leftDetect || rightDetect);
+}
 
+int convertSpeedR(int s) {
+  return 1500 - s*2;
+}
+
+int convertSpeedL(int s) {
+  return 1500 + s*2;
+}
+
+float volts(int adPin) // Measures volts at adPin
+{ // Returns floating point voltage
+  return float(analogRead(adPin)) * 5.0 / 1024.0;
+}
+
+float getAveragePhotoDetThreshold() {
+  float averageVal = (volts(leftEyePin) + volts(rightEyePin))/2;
+  return averageVal/2;
+}
+
+/*
 void detectObstacleIR() {
   // this code is not used currently
   distance=0;
@@ -132,25 +188,6 @@ int irDetect(int irLedPin, int irReceiverPin, long frequency)
   delay(1); // Down time before recheck
   return ir; // Return 1 no detect, 0 detect
 }
+*/
 
-bool detectMine() {
-  return (volts(leftEyePin) <= photoDetThreshold) || (volts(rightEyePin) <= photoDetThreshold);
-}
 
-int convertSpeedR(int s) {
-  return 1500 - s*2;
-}
-
-int convertSpeedL(int s) {
-  return 1500 + s*2;
-}
-
-float volts(int adPin) // Measures volts at adPin
-{ // Returns floating point voltage
-  return float(analogRead(adPin)) * 5.0 / 1024.0;
-}
-
-float getAveragePhotoDetThreshold() {
-  float averageVal = volts(leftEyePin) + volts(rightEyePin))/2;
-  return averageVal/2;
-}
