94493c03132fc018d1ded6aad89b996c3c4625e1
Björn Mattsson <bjorn.pm@plankter.se>
2015-04-01 09:39:10 +0200

    Added code for backing up and turning. Not tested.

--- Exercise1.ino
+++ Exercise1.ino
@@ -1,11 +1,19 @@
 #include <Servo.h>
 
 int forwardSpeed = 20; // interval: [-100, 100]
-motorLPin = 11;
-motorRPin = 12;
+int backwardSpeed = -20; // interval: [-100, 100]
+int turningSpeed = 10; // interval: [-100, 100]
+int backingTimeThreshold = 1000; // milliseconds? 1 second?
+int turningTimeThreshold = 1000; // milliseconds? 1 second?
+int motorLPin = 11;
+int motorRPin = 12;
 
 // FSM: 0 = search for mines, 1 = avoid obstacle, 2 = mine detected???
 int currentState = 0;
+int backingStartTime = 0;
+bool isCurrentlyBacking = false;
+int turningStartTime = 0;
+bool isCurrentlyTurning = false;
 
 Servo ServoL;
 Servo ServoR;
@@ -26,35 +34,81 @@ void setup() // Built-in initialization block
 
 void loop() // Main loop auto-repeats
 {
+  // get sensor readings
   bool obstacleDetected = detectObstacle();
   bool mineDetected = detectMine();
 
-  // which state should we be in?
-  if (obstacleDetected {
-    currentState = 1; // avoid the obstacle
-  } else if (mineDetected) {
-    currentState = 2; // do something with mine
-  } else {
-    currentState = 0; // search arena
-  }
 
   // Finite state machine starts here
-  if (currentState == 0) {
-    ServoL.writeMicroseconds(convertSpeed(forwardSpeed));
-    ServoR.writeMicroseconds(convertSpeed(forwardSpeed));
-  } else if (currentState == 1) {
+  if (currentState == 0) { // search the arena
+
+    if (obstacleDetected) {
+      currentState = 1; // avoid the obstacle
+    } else if (mineDetected) {
+      currentState = 2; // do something with mine
+    } else { // if nothing detected, move forward
+      ServoL.writeMicroseconds(convertSpeed(forwardSpeed));
+      ServoR.writeMicroseconds(convertSpeed(forwardSpeed));
+    }
+  } else if (currentState == 1) { // avoid the obstacle
+    stopRobot();
     // bla bla
-  } else if (currentState == 2) {
+  } else if (currentState == 2) { // do something with mine
+    stopRobot();
     // blabla
+  } else if (currentState == 3) { // back the robot up
+    if (!isCurrentlyBacking) {
+      backingStartTime = millis();
+      isCurrentlyBacking = true;
+    } else {
+      if (millis() - backingStartTime < backingTimeThreshold) {
+        ServoL.writeMicroseconds(convertSpeed(backwardSpeed));
+        ServoR.writeMicroseconds(convertSpeed(backwardSpeed));
+      } else {
+        isCurrentlyBacking = false;
+        currentState = 4; // turn the robot
+      }
+    }
+  } else if (currentState == 4) { // turn the robot
+    if (!isCurrentlyTurning) {
+      turningStartTime = millis();
+      isCurrentlyTurning = true;
+    } else {
+      if (millis() - turningStartTime < turningTimeThreshold) {
+        ServoL.writeMicroseconds(convertSpeed(turningSpeed));
+        ServoR.writeMicroseconds(convertSpeed(-turningSpeed));
+      } else {
+        isCurrentlyTurning = false;
+        currentState = 0; // move forward agaidn
+      }
+    }
   }
 
 
   delay(100);
 }
 
+void stopRobot() {
+  ServoL.writeMicroseconds(convertSpeed(0));
+  ServoR.writeMicroseconds(convertSpeed(0));
+}
+
 
 bool detectObstacle() {
   // this code uses IR sensor
+  distance=0;
+  for(frequency=38000; frequency<42000;frequency+=100){
+    int irLeft = irDetect(9, 10, frequency); // Check for object
+    distance+=irLeft;
+  }
+  Serial.println(distance);
+  //return (distance < 20);
+  return false;
+}
+
+
+void detectObstacleIR() {
+  // this code is not used currently
   distance=0;
   for(frequency=38000; frequency<42000;frequency+=100){
     int irLeft = irDetect(9, 10, frequency); // Check for object
