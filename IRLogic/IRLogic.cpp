50640111b0da19ad65846a5c332834c2c8911e00
Jared Karr <jaredkarr@users.noreply.github.com>
2015-05-06 21:03:04 +0200

    Update IRLogic.cpp

--- IRLogic/IRLogic.cpp
+++ IRLogic/IRLogic.cpp
@@ -3,14 +3,18 @@
 
 IRLogic::IRLogic() {
 	mu = 0.5;
-	sigma2 = 0.25;
+	sigma = 0.1;
 	inertia = 10.0;
 }
 
 IRLogic::~IRLogic() {}
 
 void IRLogic::setInertia(double _inertia) {
-  inertia = _inertia;
+  inertia = constrain(_inertia, 1, 1e3);
+}
+
+void IRLogic::setSigma(double _sigma) {
+  sigma = constrain(_sigma, 1e-3, 0.5);
 }
 
 double IRLogic::getState() {
@@ -23,42 +27,30 @@ unsigned int IRLogic::getFrequency() {
 
 void IRLogic::mark(uint16_t _frequency, bool _detect) {
 	
-  double a, k, p, sigma, x, z;
+  double k, x, z;
   
   // Scale frequency to unit range
   x = ((double)_frequency - nearFrequency) / (farFrequency - nearFrequency);
   
   // Rescale to z-score
-  sigma = sqrt(sigma2);
   z = (x - mu) / sigma;
   if (_detect)
     z *= -1;
 
-  // Calculate probability of measurement and
-  // information gained based on current dist
-  p = erfc(z);
-  k = -log(p);
+  // Calculate information gained wrt current distribution
+  k = log_erfc(z);
   
-  // Boundary z-score
-  a = (_detect - mu) / sigma;
-  
-  // Expected discrete observation
-  x = exp(-z * z / 2) - exp(-a * a / 2);
-  if (_detect)
-	  x = -x;
-  x *= sigma * 0.398942280401433;
-  
-  // Update variance
-  sigma2 = (inertia * sigma2 + k * x * x) / (inertia + k);
-  
-  // Un-center
-  x += mu;
   // Update mean
-//  sigma2 = (inertia * sigma2 + k * z * z * sigma2) / (inertia + k);
-  mu = (inertia * mu + k * x) / (inertia + k);
+  mu = (inertia * mu + k * _detect) / (inertia + k);
 }
 
-double erfc(double z) {
-  // approximation from (Bell 2015)
-  return 0.5 * (1 + (z>0?1:-1) * sqrt(1 - exp(-0.636619772367581 * z * z)));
+// 5th-order polynomial approximation of log(cdf(z))
+double log_erfc(double z) {  
+  double k =  9.728651322615837e-04;
+  k = k * z - 1.544435817384158e-03;
+  k = k * z - 8.843786613101896e-02;
+  k = k * z + 5.638690930288579e-01;
+  k = k * z - 1.174907902964256e+00;
+  k = k * z + 7.518621306864579e-01;
+  return k;
 }
