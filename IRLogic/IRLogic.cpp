049d2ee971a2280cfa73f11e6cde7e9e92ba1105
Jared Karr <jaredkarr@users.noreply.github.com>
2015-05-12 02:31:30 +0200

    Update IRLogic.cpp

--- IRLogic/IRLogic.cpp
+++ IRLogic/IRLogic.cpp
@@ -2,55 +2,45 @@
 #include "IRLogic.h"
 
 IRLogic::IRLogic() {
-	mu = 0.5;
-	sigma = 0.1;
-	inertia = 10.0;
+	far = 1024;
+	near = 0;
+	bias = 1;
 }
 
 IRLogic::~IRLogic() {}
 
-void IRLogic::setInertia(double _inertia) {
-  inertia = constrain(_inertia, 1, 1e3);
-}
-
-void IRLogic::setSigma(double _sigma) {
-  sigma = constrain(_sigma, 1e-3, 0.5);
-}
-
 double IRLogic::getState() {
-  return mu;
+  return (far + near) / 2;
 }
 
 unsigned int IRLogic::getFrequency() {
-	return nearFrequency + (unsigned int) (mu * (farFrequency - nearFrequency));
+	long a = max(bias, 1);
+	long b = max(-bias, 1);
+	long x = (near * a + far * b) / (a + b);
+	return x * (farFrequency - nearFrequency) / 1024 + nearFrequency;
 }
 
-void IRLogic::mark(uint16_t _frequency, bool _detect) {
-	
-  double k, x, z;
-  
-  // Scale frequency to unit range
-  x = ((double)_frequency - nearFrequency) / (farFrequency - nearFrequency);
-  
-  // Rescale to z-score
-  z = (x - mu) / sigma;
-  if (_detect)
-    z *= -1;
-
-  // Calculate information gained wrt current distribution
-  k = log_erfc(z);
-  
-  // Update mean
-  mu = (inertia * mu + k * !_detect) / (inertia + k);
+void IRLogic::mark(long _frequency, bool _detect) {
+	long x = (_frequency - nearFrequency) * 1024 / (farFrequency - nearFrequency);
+	long mu = getState();
+	if (_detect && x < far) {
+		if (x < mu) {
+			near = max(near - mu + x, 0);
+			far = mu;
+		} else far = x;
+		bias = max(1, bias + 1);
+	}
+	else if (!_detect && x > near) {
+		if (x > mu) {
+			far = min(far - mu + x, 1024);
+			near = mu;
+		} else near = x;
+		bias = min(-1, bias - 1);
+	}
 }
 
-// 5th-order polynomial approximation of log(cdf(z))
-double log_erfc(double z) {  
-  double k =  9.728651322615837e-04;
-  k = k * z - 1.544435817384158e-03;
-  k = k * z - 8.843786613101896e-02;
-  k = k * z + 5.638690930288579e-01;
-  k = k * z - 1.174907902964256e+00;
-  k = k * z + 7.518621306864579e-01;
-  return k;
+void IRLogic::reset() {
+	far = 1024;
+	near = 0;
+	bias = 1;
 }
