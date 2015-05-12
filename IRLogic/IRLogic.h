9cd3f5cc207872a404c849c856a1059e07356b41
Jared Karr <jaredkarr@users.noreply.github.com>
2015-05-12 02:32:08 +0200

    Update IRLogic.h

--- IRLogic/IRLogic.h
+++ IRLogic/IRLogic.h
@@ -11,7 +11,8 @@ class IRLogic {
     ~IRLogic();
     long getState();
     unsigned int getFrequency();
-    void mark(unsigned int _frequency, bool _detect);
+    void mark(long _frequency, bool _detect);
+    void reset();
     static const unsigned int farFrequency = 37900;
     static const unsigned int nearFrequency = 43000;
 };
